0b3bd38751e37d07959cbd00eb68f4cc5c3ae2c7
jacquesmarques <jmarques1@gmail.com>
2022-08-26 14:51:01 -0300

    Adds update action the users controller

---
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::UsersController, type: :request do
+  let(:user) { create(:user) }
+
+  describe 'GET /index' do
+
+    it 'should return user data' do
+      get api_v1_user_url(user), as: :json
+
+      expect(response.status).to eq(200)
+      json_response = JSON.parse(self.response.body)
+      expect(user.email).to eq(json_response['email'])
+    end
+
+    it 'should create user' do
+      post api_v1_users_url, params: { user: { email: 'test@test.org', password: '123456' } }, as: :json
+
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'should not create user with taken email' do
+      post api_v1_users_url, params: { user: { email: user .email, password: '123456' } }, as: :json
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it 'should update user' do
+      patch api_v1_user_url(user), params: { user: { email: user.email, password: '123456' } }, as: :json
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should not update user when invalid params are sent' do
+      patch api_v1_user_url(user), params: { user: { email: 'bad_email', password: '123456' } }, as: :json
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+  end
+end

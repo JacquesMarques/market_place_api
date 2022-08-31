0083bac63265129f394d85b5201555f34d84e131
jacquesmarques <jmarques1@gmail.com>
2022-08-31 07:51:18 -0300

    Adds some validations to products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  let(:product) { create(:product, user: user) }
+
+  it 'should have a positive price' do
+    product.price = -1
+    expect(product).to_not be_valid
+  end
 end
